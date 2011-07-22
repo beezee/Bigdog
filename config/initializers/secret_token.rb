dc6fdad0467e4949d1353032d71c76d3d5370fa4
Brian <brian.zeligson@gmail.com>
2011-07-22 09:18:25 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigdog::Application.config.secret_token = '7eb4befd0198e474b3f0706e41faf6f4dd2339adb2abe3ae5779b26385be61bf99c8afec6dad7f4fbaed660e67a37cb131a10208b4adcf30a71e23b7ae82655b'
