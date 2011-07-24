30b8776bfe8ab98575be1a9a1cb1cc5d81d09db1
Brian <brian.zeligson@gmail.com>
2011-07-23 20:36:13 -0400

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -43,4 +43,16 @@ render_views
                           "about")
     end
   end
+  
+  describe "Get 'help'" do
+    it "should be successful" do
+	 get 'help'
+	 response.should be_success
+	end
+	
+	it "should have the right title" do
+	 get 'help'
+	  response.should have_selector("title", :content => "help")
+	 end
+	end
 end
